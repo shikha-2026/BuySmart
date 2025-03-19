7beba893f8b9314ef3d80c64b86b0284a5ccfb76
shikha-2026 <shikhayadav6310@gmail.com>
2025-03-19 12:19:38 -0700

    fixx

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div className="wrapper">
+        <div className="title">Login Form</div>
+        <form action="#">
+          <div className="field">
+            <input type="text" required="" />
+            <label>Email Address</label>
+          </div>
+          <div className="field">
+            <input type="password" required="" />
+            <label>Password</label>
+          </div>
+          <div className="content">
+            <div className="checkbox">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            <div className="pass-link">
+              <a href="#">Forgot password?</a>
+            </div>
+          </div>
+          <div className="field">
+            <input type="submit" defaultValue="Login" />
+          </div>
+          <div className="signup-link">
+            Not a member? <a href="#">Signup now</a>
+          </div>
+        </form>
+      </div>
+
+    </div>
   )
 }
 
-export default Login
+export default Login;
--- frontend/src/app/(main)/signup/page.jsx
+++ frontend/src/app/(main)/signup/page.jsx
@@ -2,7 +2,78 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='flex h-screen items-center justify-center'>
+      <div className="container-signup">
+        {/* Title section */}
+        <div className="title">Registration</div>
+        <div className="content">
+          {/* Registration form */}
+          <form action="#">
+            <div className="user-details">
+              {/* Input for Full Name */}
+              <div className="input-box">
+                <span className="details">Full Name</span>
+                <input type="text" placeholder="Enter your name" required="" />
+              </div>
+              {/* Input for Username */}
+              <div className="input-box">
+                <span className="details">Username</span>
+                <input type="text" placeholder="Enter your username" required="" />
+              </div>
+              {/* Input for Email */}
+              <div className="input-box">
+                <span className="details">Email</span>
+                <input type="text" placeholder="Enter your email" required="" />
+              </div>
+              {/* Input for Phone Number */}
+              <div className="input-box">
+                <span className="details">Phone Number</span>
+                <input type="text" placeholder="Enter your number" required="" />
+              </div>
+              {/* Input for Password */}
+              <div className="input-box">
+                <span className="details">Password</span>
+                <input type="text" placeholder="Enter your password" required="" />
+              </div>
+              {/* Input for Confirm Password */}
+              <div className="input-box">
+                <span className="details">Confirm Password</span>
+                <input type="text" placeholder="Confirm your password" required="" />
+              </div>
+            </div>
+            <div className="gender-details">
+              {/* Radio buttons for gender selection */}
+              <input type="radio" name="gender" id="dot-1" />
+              <input type="radio" name="gender" id="dot-2" />
+              <input type="radio" name="gender" id="dot-3" />
+              <span className="gender-title">Gender</span>
+              <div className="category">
+                {/* Label for Male */}
+                <label htmlFor="dot-1">
+                  <span className="dot one" />
+                  <span className="gender">Male</span>
+                </label>
+                {/* Label for Female */}
+                <label htmlFor="dot-2">
+                  <span className="dot two" />
+                  <span className="gender">Female</span>
+                </label>
+                {/* Label for Prefer not to say */}
+                <label htmlFor="dot-3">
+                  <span className="dot three" />
+                  <span className="gender">Prefer not to say</span>
+                </label>
+              </div>
+            </div>
+            {/* Submit button */}
+            <div className="button">
+              <input type="submit" defaultValue="Register" />
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
